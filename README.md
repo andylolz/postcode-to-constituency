f4a8e8b677f477e4a5aa37dd197d1ee42acca14d
Andy Lulham <a.lulham@gmail.com>
2023-02-23 09:22:41 +0000

    Tidy up a bit more!

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Extracting constituencies from postcodes
 
-1. First, download the ONS postcode directory from here:
-   https://ons.maps.arcgis.com/home/search.html?t=content&q=ONS+Postcode+Directory&sortOrder=desc&sortField=modified
+1. Get some data to geocode. It should be in a CSV file, with a column named “postcode”
+2. First, download the ONS postcode directory from here:
+   https://ons.maps.arcgis.com/home/search.html?t=content&q=%22ONS+Postcode+Directory%22+-User&sortOrder=desc&sortField=modified&showFilters=true
    It’s a big zip file. Unzip it, and get the ONSPD CSV file out.
-2. Fetch constituency lookup data from here:
-   https://geoportal.statistics.gov.uk/datasets/ons::westminster-parliamentary-constituencies-december-2020-names-and-codes-in-the-united-kingdom/about
-3. Finally, download petition data
-4. Run app.py over the whole lot
+3. Fetch constituency lookup data from here:
+   https://geoportal.statistics.gov.uk/datasets/westminster-parliamentary-constituencies-dec-2020-names-and-codes-in-the-united-kingdom/about
+4. Run `python app.py [filename] --refresh`
--- app.py
+++ app.py
@@ -1,42 +1,65 @@
+import argparse
 import csv
-from collections import defaultdict
 
 
-def refresh_onspd_small():
+def refresh_onspd_lookup():
+    print("Refreshing ONSPD lookup ...")
     with open("constituencies.csv") as f:
         constituency_lookup = {
             x[1]: x[2]
             for x in csv.reader(f)}
     with open("ONSPD.csv") as fi:
-        with open("onspd_small.csv", "w") as fo:
+        with open("onspd_lookup.csv", "w") as fo:
             writer = csv.writer(fo)
             reader = csv.DictReader(fi)
-            _ = writer.writerow(("postcode", "constituency"))
+            _ = writer.writerow(("postcode", "GSS ID", "constituency name"))
             for row in reader:
                 try:
                     con = constituency_lookup[row["pcon"]]
                 except KeyError:
                     continue
-                _ = writer.writerow((row["pcd"].replace(" ", ""), con))
+                _ = writer.writerow((
+                    row["pcd"].replace(" ", ""),
+                    row["pcon"],
+                    con,
+                ))
+    print("Done.")
 
 
-with open("onspd_small.csv") as f:
-    postcode_lookup = {
-        x["postcode"]: x["constituency"]
-        for x in csv.DictReader(f)}
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Geocode a CSV of postcodes")
+    parser.add_argument(
+        "input_filename",
+        help="Input CSV file")
+    parser.add_argument(
+        "--refresh",
+        action="store_true",
+        help="Refresh ONSPD lookup")
+    args = parser.parse_args()
 
-output = defaultdict(int)
-with open("data.csv") as f:
-    reader = csv.DictReader(f)
-    for x in reader:
-        con = postcode_lookup.get(x["postcode"].replace(" ", ""))
-        if not con:
-            print("Postcode not found: {}".format(x["postcode"]))
-        # elif x["updates_opt_in"].startswith("Yes"):
-        else:
-            output[con] += 1
+    if args.refresh:
+        refresh_onspd_lookup()
 
-with open("output.csv", "w") as fh:
-    out = csv.writer(fh)
-    out.writerow(("Constituency", "Total"))
-    out.writerows(output.items())
+    with open("onspd_lookup.csv") as f:
+        postcode_lookup = {
+            x["postcode"]: x["GSS ID"]
+            for x in csv.DictReader(f)}
+
+    output = []
+    with open(args.input_filename) as f:
+        reader = csv.DictReader(f)
+        for x in reader:
+            postcode = x["postcode"].replace(" ", "")
+            constituency = postcode_lookup.get(postcode)
+            if not constituency:
+                print("Postcode not found: {}".format(x["postcode"]))
+                continue
+            else:
+                postcode = "{outcode} {incode}".format(
+                    outcode=postcode[:-3], incode=postcode[-3:])
+            output.append((x["id"], postcode, constituency))
+
+    with open("output.csv", "w") as fh:
+        out = csv.writer(fh)
+        out.writerow(("pk", "postcode", "constituency"))
+        out.writerows(output)
